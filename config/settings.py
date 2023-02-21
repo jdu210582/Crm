b2888c3c144548b9248ccf950cd7ede3860b8d35
jdu210582 <210582j@jdu.uz>
2023-02-21 21:22:25 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+ALLOWED_HOSTS = ['maincrm.herokuapp.com','maincrm.herokuapp.com/admin']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -126,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR / "static")
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR / "staticfiles"),
 )
-
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
